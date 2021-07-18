b335a71906f0eb6973cb3ce5bf34079bf5525260
Daniel <daniel@pink-taco.local>
2021-07-18 19:41:35 -0400

    solved the string array output

---
+++ easy_3/exercise2.js
@@ -0,0 +1,7 @@
+// Question 2
+// What will the following code output?
+
+console.log([1, 2, 3] + [4, 5]);
+
+//
+//the + operator converts the arrays to strings, and then concatenates to strings, so the output is the string 1,2,34, 5
