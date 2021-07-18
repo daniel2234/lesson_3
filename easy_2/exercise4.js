6af6b16958d859beb27c01cdc8517ebdacd65ad6
Daniel <daniel@pink-taco.local>
2021-07-18 15:36:31 -0400

    added two different ways for add strings together

---
+++ easy_2/exercise4.js
@@ -0,0 +1,14 @@
+// Question 4
+// Starting with the string:
+
+let famousWords = "seven years ago...";
+
+// show two different ways to put the expected "Four score and " in front of it.
+
+let firstFamousWords = "Four score and "
+
+// firstFamousWords += famousWords;
+
+console.log(firstFamousWords += famousWords);
+
+
