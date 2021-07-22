cc223daad00b618c52d1f0f74f483dedbe2424ba
Daniel <daniel@pink-taco.local>
2021-07-22 10:48:29 -0400

    learned a floating point number handling in javascript

---
+++ medium_1/exercise5.js
@@ -0,0 +1,8 @@
+// Question 5
+// What will the following two lines of code output?
+
+console.log(0.3 + 0.6); //0.9 //=> Actual answer is 0.8999999
+console.log(0.3 + 0.6 === 0.9); //true => actual answer false
+
+// Don't look at the solution before you answer.
+//Most floating point representations used on computers lack a certain amount of precision, and that can yield unexpected results like these
