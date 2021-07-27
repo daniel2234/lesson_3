de6cc54a4b9b4d50328411d916c0c849a5a3c75c
Daniel <daniel@pink-taco.local>
2021-07-27 10:46:07 -0400

    added reason for data mutation

---
+++ medium_1/exercise4.js
@@ -0,0 +1,14 @@
+// Question 4
+// Alyssa was asked to write an implementation of a rolling buffer.
+// You can add and remove elements from a rolling buffer. 
+//However, once the buffer becomes full, any new elements will displace the oldest elements in the buffer.
+
+// She wrote two implementations of the code for adding elements to the buffer. 
+//In presenting the code to her team leader, 
+//she said "Take your pick. Do you prefer push() or concat() for modifying the buffer?".
+
+// Is there a difference between these implementations, 
+//other than the method she used to add an element to the buffer?
+
+/* TODO */
+
